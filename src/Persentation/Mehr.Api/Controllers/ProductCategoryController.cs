0f08c82707dbd9eafdd4d8238b09801d4085f397
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-14 23:06:43 +0200

    add endpoint for product repository

---
+++ src/Persentation/Mehr.Api/Controllers/ProductCategoryController.cs
@@ -0,0 +1,24 @@
+﻿using Mehr.Application.Intrefaces;
+using Mehr.Domain.Dtos.ProdcutCategory;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mehr.Api.Controllers;
+
+[ApiController]
+[Route("api/productCategories")]
+public class ProductCategoryController : Controller
+{
+    private readonly IProductCategoryService _service;
+
+    public ProductCategoryController(IProductCategoryService service)
+    {
+        _service = service;
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> AddAsync(AddProductCategoryDto dto)
+    {
+        var result = await _service.AddAsync(dto);
+        return Ok(result);
+    }
+}
