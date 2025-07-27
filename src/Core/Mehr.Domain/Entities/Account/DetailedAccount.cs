f12a8c7d049fa5ec660b96b6153d0af293066ac9
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-27 16:35:59 +0200

    Add detailed category account entity

---
+++ src/Core/Mehr.Domain/Entities/Account/DetailedAccount.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mehr.Domain.Entities.Account
+{
+    internal class DetailedAccount
+    {
+    }
+}
---
+++ src/Core/Mehr.Domain/Entities/Account/DetailedCategoryAccount.cs
@@ -0,0 +1,17 @@
+﻿namespace Mehr.Domain.Entities.Account;
+
+public class DetailedCategoryAccount
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public DetailedCategoryType Type { get; set; }
+    public bool Private { get; set; }
+    public bool IsDetailedCategory { get; set; }
+}
+
+public enum DetailedCategoryType
+{
+    Permanet = 1,
+    Temporaty = 2,
+    Control = 3
+}
