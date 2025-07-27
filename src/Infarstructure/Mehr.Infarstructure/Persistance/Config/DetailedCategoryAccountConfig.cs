3ea81872c9b756dc43b79562ff2bba926b3c2121
alirezadoostdar <alireza.doostdar@gmail.com>
2025-07-27 16:47:01 +0200

    add config file for detailed category config

---
+++ src/Infarstructure/Mehr.Infarstructure/Persistance/Config/DetailedCategoryAccountConfig.cs
@@ -0,0 +1,35 @@
+﻿using Mehr.Domain.Entities.Account;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Persistance.Config;
+
+public class DetailedCategoryAccountConfig : IEntityTypeConfiguration<DetailedCategoryAccount>
+{
+    public void Configure(EntityTypeBuilder<DetailedCategoryAccount> builder)
+    {
+        builder.ToTable("CodeMNGUPTbl");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("CodeMNGUPTbl")
+            .IsRequired();
+
+        builder.Property(x => x.Title)
+            .HasColumnName("Title")
+            .IsRequired();
+
+        builder.Property(x => x.Type)
+            .HasColumnName("Kind")
+            .IsRequired();
+
+        builder.Property(x => x.Private)
+            .HasColumnName("Private");
+
+        builder.Property(x => x.IsDetailedCategory)
+            .HasColumnName("IsTafsilGroup")
+            .IsRequired();
+    }
+}
