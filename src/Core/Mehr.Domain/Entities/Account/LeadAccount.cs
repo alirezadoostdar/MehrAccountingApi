807426f58f592dd0ac3439c146a34e43641a98b3
MehrAccounting <mehr.group.software@gmail.com>
2025-08-11 00:24:14 +0200

    change lead account entity

--- src/Core/Mehr.Domain/Entities/Account/LeadAccount.cs
+++ src/Core/Mehr.Domain/Entities/Account/LeadAccount.cs
@@ -1,12 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Mehr.Domain.Entities
+﻿namespace Mehr.Domain.Entities.Account;
+public class LeadAccount
 {
-    internal class LeadAccount
-    {
-    }
+    public int Id { get; set; }
+    public int ParentId { get; set; }
+    public LeadAccountType Type { get; set; }
+    public string Code { get; set; }
+    public string Title { get; set; }
+    public bool IsActive { get; set; }
+    public bool IsLock { get; set; }
+    public decimal Balance { get; set; }
+    public SecureLevel SecureLevel { get; set; }
+    public bool HasDetailedCategory { get; set; }
+    public int DetailedId { get; set; }
+    public int DetailedCategoryId { get; set; }
+    public bool IsTemp { get; set; }
+
+}
+
+public enum LeadAccountType
+{
+    ZeroAccount = 0,
+    FirstLevel = 1,
+    SecondLevel = 2,
+    ThirdLevel = 3,
+    ForthLevel = 4,
+    FifthLevel = 5
+}
+
+
+public enum SecureLevel
+{
+    First = 0,
+    Second = 1,
+    Third = 2,
+    Forth = 3
 }
