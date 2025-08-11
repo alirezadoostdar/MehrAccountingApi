4a8d9ca37c9a08b1a661f89f17a423f165767ede
alirezadoostdar <alireza.doostdar@gmail.com>
2025-08-11 23:19:46 +0200

    implement unit of work pattern

---
+++ src/Core/Mehr.Application/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿namespace Mehr.Application;
+
+public interface IUnitOfWork
+{
+    void SaveChange();
+    Task SaveChangesAsync();
+    void Begin();
+    Task BeginAsync();
+    void Commit();
+    Task CommitAsync();
+    void Rollback();
+    Task RollbackAsync();
+}
---
+++ src/Infarstructure/Mehr.Infarstructure/UnitOfWork.cs
@@ -0,0 +1,47 @@
+﻿using Mehr.Application;
+
+namespace Mehr.Infarstructure;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly ApplicationDbContext _context;
+    public void Begin()
+    {
+        _context.Database.BeginTransaction();
+    }
+
+    public async Task BeginAsync()
+    {
+       await _context.Database.BeginTransactionAsync();
+    }
+
+    public void Commit()
+    {
+        _context.Database.CommitTransaction();
+    }
+
+    public async Task CommitAsync()
+    {
+        await _context.Database.CommitTransactionAsync();
+    }
+
+    public void Rollback()
+    {
+        _context.Database.RollbackTransaction();
+    }
+
+    public async Task RollbackAsync()
+    {
+        await _context.Database.RollbackTransactionAsync();
+    }
+
+    public void SaveChange()
+    {
+        _context.SaveChanges();
+    }
+
+    public async Task SaveChangesAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
