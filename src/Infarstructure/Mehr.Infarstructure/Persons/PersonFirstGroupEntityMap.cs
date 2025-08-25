a46db468af4e040501ee7b0884bdf2ef183263a2
alirezadoostdar <alireza.doostdar@gmail.com>
2025-08-25 23:47:37 +0200

    add entity map for person first group

---
+++ src/Infarstructure/Mehr.Infarstructure/Persons/PersonFirstGroupEntityMap.cs
@@ -0,0 +1,42 @@
+﻿using Mehr.Domain.Entities.Persons;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Persons;
+
+public class PersonFirstGroupEntityMap : IEntityTypeConfiguration<PersonFirstGroup>
+{
+    public void Configure(EntityTypeBuilder<PersonFirstGroup> builder)
+    {
+        builder.ToTable("PersonGroup1Tbl");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("GroupId");
+
+        builder.Property(x => x.Title)
+            .HasMaxLength(100)
+            .HasColumnName("GroupName")
+            .IsRequired();
+    }
+}
+
+
+public class PersonSecondGroupEntityMap : IEntityTypeConfiguration<PersonSecondGroup>
+{
+    public void Configure(EntityTypeBuilder<PersonSecondGroup> builder)
+    {
+        builder.ToTable("PersonGroup1Tbl");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("GroupId");
+
+        builder.Property(x => x.Title)
+            .HasMaxLength(100)
+            .HasColumnName("GroupName")
+            .IsRequired();
+    }
+}
