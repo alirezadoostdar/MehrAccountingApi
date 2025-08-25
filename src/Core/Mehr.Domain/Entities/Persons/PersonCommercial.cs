305e59fdfeef93a118747fdb380f3f752fbaa94a
alirezadoostdar <alireza.doostdar@gmail.com>
2025-08-25 23:53:36 +0200

    add person commercial entity

---
+++ src/Core/Mehr.Domain/Entities/Persons/PersonCommercial.cs
@@ -0,0 +1,9 @@
+﻿namespace Mehr.Domain.Entities.Persons;
+
+public class PersonCommercial
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int Color { get; set; }
+    public string HexaColor { get; set; }
+}
