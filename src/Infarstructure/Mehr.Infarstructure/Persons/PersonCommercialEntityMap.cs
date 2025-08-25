316d0b31c155dd714a15d257004e18f3bdee51ec
alirezadoostdar <alireza.doostdar@gmail.com>
2025-08-25 23:58:42 +0200

    add entity map for person commercial

---
+++ src/Infarstructure/Mehr.Infarstructure/Persons/PersonCommercialEntityMap.cs
@@ -0,0 +1,25 @@
+﻿using Mehr.Domain.Entities.Persons;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Persons;
+
+public class PersonCommercialEntityMap : IEntityTypeConfiguration<PersonCommercial>
+{
+    public void Configure(EntityTypeBuilder<PersonCommercial> builder)
+    {
+        builder.ToTable("PersonCommercialTBL");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("ID");
+
+        builder.Property(x => x.Title)
+            .HasColumnName("Title")
+            .HasMaxLength(200);
+
+        builder.Property(x => x.HexaColor)
+            .HasMaxLength(15);
+    }
+}
