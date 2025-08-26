844106d369ad7225a5431ee5a436353eeada2e73
alirezadoostdar <alireza.doostdar@gmail.com>
2025-08-26 23:17:35 +0200

    add entity map for product group 2

---
+++ src/Infarstructure/Mehr.Infarstructure/Stocks/ProductGroup2EntityMap.cs
@@ -0,0 +1,26 @@
+﻿using Mehr.Domain.Entities.Stocks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Stocks;
+
+public class ProductGroup2EntityMap : IEntityTypeConfiguration<ProductGroup2>
+{
+    public void Configure(EntityTypeBuilder<ProductGroup2> builder)
+    {
+        builder.ToTable("stockGroup2Tbl");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("GroupId");
+
+        builder.Property(x => x.Title)
+            .HasColumnName("GroupName")
+            .HasMaxLength(100)
+            .IsRequired();
+
+        builder.Property(x => x.VisitPercent)
+            .HasColumnName("VisitPercent");
+    }
+}
