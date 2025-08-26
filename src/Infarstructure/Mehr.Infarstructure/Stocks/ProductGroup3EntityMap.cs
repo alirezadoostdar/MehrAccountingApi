41b9b0a004b3b382af7dd5bbed93d45ce45e097c
alirezadoostdar <alireza.doostdar@gmail.com>
2025-08-26 23:18:59 +0200

    add entity map for product group 3

---
+++ src/Infarstructure/Mehr.Infarstructure/Stocks/ProductGroup3EntityMap.cs
@@ -0,0 +1,23 @@
+﻿using Mehr.Domain.Entities.Stocks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Stocks;
+
+public class ProductGroup3EntityMap : IEntityTypeConfiguration<ProductGroup3>
+{
+    public void Configure(EntityTypeBuilder<ProductGroup3> builder)
+    {
+        builder.ToTable("stockGroup3Tbl");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("GroupId");
+
+        builder.Property(x => x.Title)
+            .HasColumnName("GroupName")
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+}
