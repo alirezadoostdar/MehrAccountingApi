a77c4a3c2a935def047a76c87cfbdab2dda2fb12
alirezadoostdar <alireza.doostdar@gmail.com>
2025-08-28 23:46:09 +0200

    add entity for bank

--- src/Core/Mehr.Domain/Entities/Accounts/DetailedAccount.cs
+++ src/Core/Mehr.Domain/Entities/Accounts/DetailedAccount.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Mehr.Domain.Entities.Accounts;
 
-namespace Mehr.Domain.Entities.Accounts
+public class DetailedAccount
 {
-    internal class DetailedAccount
-    {
-    }
+
 }
---
+++ src/Core/Mehr.Domain/Entities/Banks/Bank.cs
@@ -0,0 +1,16 @@
+﻿using Mehr.Domain.Entities.Accounts;
+
+namespace Mehr.Domain.Entities.Banks;
+
+public class Bank : DetailedAccount
+{
+    public string AccountNumber { get; set; }
+    public string Manager { get; set; }
+    public int ContactId { get; set; }
+    public string Comment { get; set; }
+    public string CardNumber { get; set; }
+    public string Iban { get; set; }
+    public string PaySwitchNumber { get; set; }
+    public string ShoppingNumber { get; set; }
+    public string TerminalNumber { get; set; }
+}
