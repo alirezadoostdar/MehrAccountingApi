513178bd1978591afda34e42c0668032c8e9bd13
alirezadoostdar <alireza.doostdar@gmail.com>
2025-09-01 22:53:44 +0200

    add entity map for cost first group

---
+++ src/Infarstructure/Mehr.Infarstructure/Costs/CostFirstGroupEntityMap.cs
@@ -0,0 +1,25 @@
+﻿using Mehr.Domain.Entities.Banks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Conventions;
+
+namespace Mehr.Infarstructure.Costs;
+
+public class CostFirstGroupEntityMap : IEntityTypeConfiguration<CostFirstGroup>
+{
+    public void Configure(EntityTypeBuilder<CostFirstGroup> builder)
+    {
+        builder.ToTable("CostGroup1Tbl");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+           .HasColumnName("GroupId")
+           .IsRequired();
+
+        builder.Property(x => x.Title)
+            .HasColumnName("GroupName")
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+}
