99ca0f6a916f4e3450dd884d57e1670666257768
alirezadoostdar <alireza.doostdar@gmail.com>
2025-09-01 23:12:57 +0200

    complete detaile account entity

--- src/Core/Mehr.Domain/Entities/Accounts/DetailedAccount.cs
+++ src/Core/Mehr.Domain/Entities/Accounts/DetailedAccount.cs
@@ -2,5 +2,20 @@
 
 public class DetailedAccount
 {
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public int CategoryId { get; set; }
+    public DetailedCategoryAccount Category { get; set; }
+    public SecureLevelType SecureLevel { get; set; }
+    public bool IsDebtor { get; set; }
+    public bool IsUpdate { get; set; }
 
 }
+public enum SecureLevelType
+{
+    First = 0,
+    Second = 1,
+    Third = 2,
+    Forth = 3
+}
+
---
+++ src/Infarstructure/Mehr.Infarstructure/Costs/CostEntityMap.cs
@@ -0,0 +1,25 @@
+﻿using Mehr.Domain.Entities.Costs;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Costs;
+
+public class CostEntityMap : IEntityTypeConfiguration<Cost>
+{
+    public void Configure(EntityTypeBuilder<Cost> builder)
+    {
+        builder.ToTable("CostTbl");
+
+        builder.HasKey("Fk_AccountSyscode");
+
+        builder.Property(x => x.FirstGroupId)
+           .HasColumnName("GroupID1");
+
+        builder.Property(x => x.SecondGroupId)
+            .HasColumnName("GroupID1");
+
+        builder.Property(x => x.Comment)
+            .HasMaxLength(1000)
+            .IsRequired();
+    }
+}
