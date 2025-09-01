7909ea97418ed5993223f2fab9e006ca022af0b3
alirezadoostdar <alireza.doostdar@gmail.com>
2025-09-01 22:54:52 +0200

    add entity map for cost second group

---
+++ src/Infarstructure/Mehr.Infarstructure/Costs/CostSecondGroupEntityMap.cs
@@ -0,0 +1,24 @@
+﻿using Mehr.Domain.Entities.Banks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Costs;
+
+public class CostSecondGroupEntityMap : IEntityTypeConfiguration<CostSecondGroup>
+{
+    public void Configure(EntityTypeBuilder<CostSecondGroup> builder)
+    {
+        builder.ToTable("CostGroup2Tbl");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+           .HasColumnName("GroupId")
+           .IsRequired();
+
+        builder.Property(x => x.Title)
+            .HasColumnName("GroupName")
+            .HasMaxLength(100)
+            .IsRequired();
+    }
+}
