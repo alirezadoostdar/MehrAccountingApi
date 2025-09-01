9ccd5a14ce2cd4940aa79ca6d228e1462ee31945
alirezadoostdar <alireza.doostdar@gmail.com>
2025-09-01 22:46:40 +0200

    add cost entity

--- src/Core/Mehr.Domain/Entities/Costs/Cost.cs
+++ src/Core/Mehr.Domain/Entities/Costs/Cost.cs
@@ -1,8 +1,17 @@
 ﻿using Mehr.Domain.Entities.Accounts;
+using Mehr.Domain.Entities.Banks;
 
 namespace Mehr.Domain.Entities.Costs;
 
 public class Cost : DetailedAccount
 {
     public int MyProperty { get; set; }
+
+    public int FirstGroupId { get; set; }
+    public CostFirstGroup FirstGroup { get; set; }
+
+    public int SecondGroupId { get; set; }
+    public CostSecondGroup SecondGroup { get; set; }
+
+    public string Comment { get; set; }
 }
