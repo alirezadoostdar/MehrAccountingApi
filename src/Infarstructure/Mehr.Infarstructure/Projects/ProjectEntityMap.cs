966bb8b3db38f0e7edb15a8451435285f1368300
alirezadoostdar <alireza.doostdar@gmail.com>
2025-09-07 22:43:57 +0200

    add entity map for project

---
+++ src/Infarstructure/Mehr.Infarstructure/Projects/ProjectEntityMap.cs
@@ -0,0 +1,25 @@
+﻿using Mehr.Domain.Entities.Projects;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Projects;
+
+public class ProjectEntityMap : IEntityTypeConfiguration<Project>
+{
+    public void Configure(EntityTypeBuilder<Project> builder)
+    {
+        builder.ToTable("ProjectTBL");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("ID");
+
+        builder.Property(x => x.Title)
+            .HasMaxLength(500)
+            .IsRequired();
+
+        builder.Property(x => x.Active)
+            .IsRequired();
+    }
+}
