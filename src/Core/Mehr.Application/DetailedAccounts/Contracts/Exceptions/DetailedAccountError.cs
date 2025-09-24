fdb51d2ae47dfd8e0c3c2ba9a7a3acff9a204961
alirezadoostdar <alireza.doostdar@gmail.com>
2025-09-24 23:34:55 +0200

    refactore detailed account error class

--- src/Core/Mehr.Application/DetailedAccounts/Contracts/Exceptions/DetailedAccountError.cs
+++ src/Core/Mehr.Application/DetailedAccounts/Contracts/Exceptions/DetailedAccountError.cs
@@ -7,6 +7,6 @@ namespace Mehr.Application.DetailedAccounts.Contracts.Exceptions;
 public static class DetailedAccountError
 {
     public static Error NotFound(int id) => Error.NotFound(
-        "DetaileAccount.NotFound",
+        "510",
         $"The detaileAccount with the Id = '{id}' was not found");
 }
