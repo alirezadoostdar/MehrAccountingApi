9ed26883f12d4774b8098c76e01b59c891575f2c
alirezadoostdar <alireza.doostdar@gmail.com>
2025-09-26 23:30:44 +0200

    Add cost dto

---
+++ src/Core/Mehr.Application/Costs/Contracts/Dtos/AddCostDto.cs
@@ -0,0 +1,13 @@
+﻿using Mehr.Domain.Entities.Accounts;
+
+namespace Mehr.Application.Costs.Contracts.Dtos;
+
+public class AddCostDto
+{
+    public string Title { get; set; }
+    public int CategoryId { get; set; }
+    public decimal CreditLimit { get; set; }
+    public SecureLevelType SecureLevel { get; set; }
+    public bool IsDebtor { get; set; }
+    public int GroupId1 { get; set; }
+}
