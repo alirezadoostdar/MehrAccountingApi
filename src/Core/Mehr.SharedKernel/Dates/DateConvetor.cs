e4c9e7b9315a3cdd5a9b519d7d004249d644924a
alirezadoostdar <alireza.doostdar@gmail.com>
2025-11-18 20:43:03 +0100

    add date convertor class

---
+++ src/Core/Mehr.SharedKernel/Dates/DateConvetor.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Mehr.SharedKernel.Dates;
+
+public static class DateConvetor
+{
+    public static string PersianDate(this DateTime gregorianDate)
+    {
+        var pc = new PersianCalendar();
+
+        int year = pc.GetYear(gregorianDate);
+        int month = pc.GetMonth(gregorianDate);
+        int day = pc.GetDayOfMonth(gregorianDate);
+
+        return $"{year}/{month.ToString("00")}/{day.ToString("00")}";
+    }
+}
