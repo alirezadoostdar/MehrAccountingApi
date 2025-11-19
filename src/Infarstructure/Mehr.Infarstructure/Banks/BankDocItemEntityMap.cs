4c30a7c361d8b4e2a5796f93e37090afc54e8907
alirezadoostdar <alireza.doostdar@gmail.com>
2025-11-19 22:16:50 +0100

    add bank doc item entity map

---
+++ src/Infarstructure/Mehr.Infarstructure/Banks/BankDocItemEntityMap.cs
@@ -0,0 +1,24 @@
+﻿using Mehr.Domain.Banks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Banks;
+
+public class BankDocItemEntityMap : IEntityTypeConfiguration<BankDocItem>
+{
+    public void Configure(EntityTypeBuilder<BankDocItem> builder)
+    {
+        builder.ToTable("BankDetailTbl", tb =>
+        {
+            tb.Property(e => e.Id).HasColumnName("Fk_DocDetailsyscode");
+        });
+
+        builder.Property(x => x.TransactionNumber)
+            .HasColumnName("Number")
+            .HasMaxLength(1000)
+            .IsRequired();
+
+        builder.Property(x => x.IsPos)
+            .IsRequired();
+    }
+}
