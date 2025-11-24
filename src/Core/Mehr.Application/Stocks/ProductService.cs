602292b7123d3666a4761353e62847ff61481bfe
alirezadoostdar <alireza.doostdar@gmail.com>
2025-11-24 21:46:31 +0100

    add interface for product repository

---
+++ src/Core/Mehr.Application/Stocks/ProductService.cs
@@ -0,0 +1,14 @@
+﻿using Mehr.Application.Stocks.Contracts;
+using Mehr.Domain.Stocks;
+using Mehr.SharedKernel;
+
+namespace Mehr.Application.Stocks;
+
+internal class ProductService : IProductService
+{
+    private readonly Iproductre
+    public Task<Result<List<Product>>> GetAllAsync(CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ src/Core/Mehr.Domain/Stocks/Contracts/IProductRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Mehr.Domain.Stocks.Contracts;
+
+public interface IProductRepository
+{
+    Task<List<Product>> GetAllAsync(CancellationToken cancellationToken);
+}
