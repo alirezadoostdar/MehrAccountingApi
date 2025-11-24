41a4569d1a3b20a4faa308b538874184e7158dc0
alirezadoostdar <alireza.doostdar@gmail.com>
2025-11-24 21:41:56 +0100

    add interface for product service

---
+++ src/Core/Mehr.Application/Stocks/Contracts/IProductService.cs
@@ -0,0 +1,9 @@
+﻿using Mehr.Domain.Stocks;
+using Mehr.SharedKernel;
+
+namespace Mehr.Application.Stocks.Contracts;
+
+public interface IProductService
+{
+    Task<Result<List<Product>>> GetAllAsync(CancellationToken cancellationToken);
+}
---
+++ src/Persentation/Mehr.Api/Controllers/ProductController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Mehr.Api.Controllers
+{
+    [ApiController]
+    [Route("api/products")]
+    public class ProductController : Controller
+    {
+        
+    }
+}
