aa5df779de446b396de6707c1160122e21fa3015
alirezadoostdar <alireza.doostdar@gmail.com>
2025-12-31 13:22:55 +0100

    add authentication controller

---
+++ src/Persentation/Mehr.Api/Controllers/AuthenticationController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+
+namespace Mehr.Api.Controllers;
+
+[ApiController]
+[Route("api/authentication")]
+public class AuthenticationController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
