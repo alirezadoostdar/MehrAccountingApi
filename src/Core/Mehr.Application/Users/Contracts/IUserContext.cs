98efc0f2d882a69d7bd45ac2fda07da57440f394
alirezadoostdar <alireza.doostdar@gmail.com>
2026-01-21 22:27:15 +0100

    add user context interface

---
+++ src/Core/Mehr.Application/Users/Contracts/IUserContext.cs
@@ -0,0 +1,9 @@
+﻿namespace Mehr.Application.Users.Contracts;
+
+public interface IUserContext
+{
+    int UserId { get; }
+    int GroupId { get; }
+    string UserName { get; }
+    int SecureLevel { get; }
+}
