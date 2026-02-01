479db236940b0938c80bb666727a6f3313384362
alirezadoostdar <alireza.doostdar@gmail.com>
2026-02-01 21:09:05 +0100

    add cache service

---
+++ src/Core/Mehr.Application/Common/Contracts/ICacheService.cs
@@ -0,0 +1,24 @@
+﻿namespace Mehr.Application.Common.Contracts;
+
+public interface ICacheService
+{
+    Task<T?> GetAsync<T>(string cacheKey, CancellationToken cancellationToken);
+
+    Task<T> GetOrCreateAsync<T>(
+        string key,
+        Func<CancellationToken, Task<T>> factory,
+        TimeSpan? absoluteExpirationRelativeToNow = null,
+        TimeSpan? slidingExpiration = null,
+        CancellationToken cancellationToken = default);
+
+    Task<T> SetAsync<T>(
+        string key,
+        T value,
+        TimeSpan? absoluteExpirationRelativeToNow = null,
+        TimeSpan? slidingExpiration = null,
+        CancellationToken cancellationToken = default);
+
+    Task RemoveAsync(string key, CancellationToken cancellationToken= default);
+
+
+}
