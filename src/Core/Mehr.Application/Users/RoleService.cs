8a63f4404abee2a32ca8bd4a5980cf2f8a4420c3
alirezadoostdar <alireza.doostdar@gmail.com>
2026-02-02 20:59:37 +0100

    add role service

---
+++ src/Core/Mehr.Application/Users/RoleService.cs
@@ -0,0 +1,22 @@
+﻿using Mehr.Application.Users.Contracts;
+using Mehr.Domain.Users.Contracts;
+
+namespace Mehr.Application.Users;
+
+public class RoleService : IRoleService
+{
+    private readonly IRoleRepository _repository;
+
+    public RoleService(IRoleRepository repository)
+    {
+        _repository = repository;
+    }
+
+    public async Task<HashSet<string>> GetPolicyList(int roleId, CancellationToken cancellationToken)
+    {
+        var policyList = await _repository.GetPolicyList(roleId, cancellationToken);
+        return policyList.Where(x => x.Value == 1)
+            .Select(x => x.Id.ToString())
+            .ToHashSet();
+    }
+}
