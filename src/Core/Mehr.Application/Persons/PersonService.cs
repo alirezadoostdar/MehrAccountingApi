8cc56b79e7db760c6583a5c4b1e95364bb1d07c9
alirezadoostdar <alireza.doostdar@gmail.com>
2026-02-08 22:03:29 +0100

    add person service interface

---
+++ src/Core/Mehr.Application/Persons/PersonService.cs
@@ -0,0 +1,23 @@
+﻿using Mehr.Application.Persons.Contracts;
+using Mehr.Domain.Persons;
+using Mehr.Domain.Persons.Contracts;
+using Mehr.SharedKernel;
+
+namespace Mehr.Application.Persons;
+
+public class PersonService : IPersonService
+{
+    private readonly IPersonRepository _repository;
+    private readonly IUnitOfWork _unitOfWork;
+
+    public PersonService(IPersonRepository repository, IUnitOfWork unitOfWork)
+    {
+        _repository = repository;
+        _unitOfWork = unitOfWork;
+    }
+
+    public Task<Result<Person>> GetByIdAsync(int id, CancellationToken cancellationToken)
+    {
+        throw new NotImplementedException();
+    }
+}
