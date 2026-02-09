c5b50db9ef930a8de6030d1aea39a430a9db1b3a
alirezadoostdar <alireza.doostdar@gmail.com>
2026-02-09 21:49:25 +0100

    create entity map for person sale price

---
+++ src/Infarstructure/Mehr.Infarstructure/Persons/PersonSalePriceEntityMap.cs
@@ -0,0 +1,24 @@
+﻿using Mehr.Domain.Persons;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Mehr.Infarstructure.Persons;
+
+public class PersonSalePriceEntityMap : IEntityTypeConfiguration<PersonSalePrice>
+{
+    public void Configure(EntityTypeBuilder<PersonSalePrice> builder)
+    {
+        builder.ToTable("InfoPersonPriceNoTbl");
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("Value")
+            .IsRequired();
+
+        builder.Property(x => x.Title)
+            .HasColumnName("Caption")
+            .HasMaxLength(1000)
+            .IsRequired();
+    }
+}
