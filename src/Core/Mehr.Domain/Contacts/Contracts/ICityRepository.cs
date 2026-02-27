b08ec13d3fbeecc6f213036a68589614065efa2b
alirezadoostdar <alireza.doostdar@gmail.com>
2026-02-27 21:59:53 +0100

    add city repository

---
+++ src/Core/Mehr.Domain/Contacts/Contracts/ICityRepository.cs
@@ -0,0 +1,8 @@
+﻿using Mehr.Domain.Contacts.Dtos;
+
+namespace Mehr.Domain.Contacts.Contracts;
+
+public interface ICityRepository
+{
+    Task<List<GetCityDto>> GetAllAsync(int stateId, CancellationToken cancellationToken);
+}
---
+++ src/Core/Mehr.Domain/Contacts/Dtos/GetCityDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Mehr.Domain.Contacts.Dtos;
+
+public class GetCityDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+}
