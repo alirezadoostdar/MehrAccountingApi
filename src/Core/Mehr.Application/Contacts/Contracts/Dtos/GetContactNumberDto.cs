3ae1d616230e954366d98439cb332f327736c01c
alirezadoostdar <alireza.doostdar@gmail.com>
2026-03-02 22:14:27 +0100

    Add contact number dto

---
+++ src/Core/Mehr.Application/Contacts/Contracts/Dtos/GetContactNumberDto.cs
@@ -0,0 +1,14 @@
+﻿using Mehr.Domain.Contacts;
+
+namespace Mehr.Application.Contacts.Contracts.Dtos;
+
+public class GetContactNumberDto
+{
+    public int Id { get; set; }
+    public string Number { get; set; }
+    public string Title { get; set; }
+
+    public byte ContactTypeId { get; set; }
+    public string ContactType { get; set; }
+
+}
