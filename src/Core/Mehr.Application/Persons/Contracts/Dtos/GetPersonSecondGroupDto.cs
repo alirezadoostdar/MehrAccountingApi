102ebd3ed6e54c6f3e13397e0667bd2f4b985a24
alirezadoostdar <alireza.doostdar@gmail.com>
2026-03-17 21:19:58 +0100

    refactor person service interface

---
+++ src/Core/Mehr.Application/Persons/Contracts/Dtos/GetPersonSecondGroupDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Mehr.Application.Persons.Contracts.Dtos;
+
+public class GetPersonSecondGroupDto
+{
+    public int Id { get; set; }
+    public string title { get; set; } = null!;
+}
--- src/Core/Mehr.Application/Persons/Contracts/IPersonService.cs
+++ src/Core/Mehr.Application/Persons/Contracts/IPersonService.cs
@@ -13,4 +13,10 @@ public interface IPersonService
     Task<Result<int>> AddFirstGroupAsync(AddPersonFirstGroupDto dto, CancellationToken cancellationToken);
     Task<Result<bool>> DeleteFirstGroupAsync(int id, CancellationToken cancellationToken);
     Task<Result<bool>> UpdateFirstGroupAsync(int id, UpdatePersonFirstGroupDto dto, CancellationToken cancellationToken);
+
+    Task<Result<GetPersonFirstGroupDto>> GetSecondGroupByIdAsync(int id, CancellationToken cancellationToken);
+    Task<Result<List<GetPersonFirstGroupDto>>> GetAllSecondGroupAsync(CancellationToken cancellationToken);
+    Task<Result<int>> AddSecondGroupAsync(AddPersonFirstGroupDto dto, CancellationToken cancellationToken);
+    Task<Result<bool>> DeleteSecondGroupAsync(int id, CancellationToken cancellationToken);
+    Task<Result<bool>> UpdateSecondGroupAsync(int id, UpdatePersonFirstGroupDto dto, CancellationToken cancellationToken);
 }
