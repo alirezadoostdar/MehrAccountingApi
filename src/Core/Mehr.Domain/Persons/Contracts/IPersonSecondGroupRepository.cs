caf32f27cb46f59048e4a7a3fefb3e551a103d83
alirezadoostdar <alireza.doostdar@gmail.com>
2026-03-17 21:16:30 +0100

    add person second group repository

---
+++ src/Core/Mehr.Domain/Persons/Contracts/IPersonSecondGroupRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace Mehr.Domain.Persons.Contracts;
+
+public interface IPersonSecondGroupRepository
+{
+    Task<PersonSecondGroup?> GetByIdAsync(int id, CancellationToken cancellationToken);
+    Task<PersonSecondGroup?> GetByTitleAsync(string title, CancellationToken cancellationToken);
+    Task<PersonSecondGroup?> GetByIdNoTarackAsync(int id, CancellationToken cancellationToken);
+    Task<List<PersonSecondGroup>> GetAllAsync(CancellationToken cancellationToken);
+    Task<bool> IsUsed(int id, CancellationToken cancellationToken);
+    Task AddFirstGroupAsync(PersonSecondGroup secondGroup, CancellationToken cancellationToken);
+    void Delete(PersonSecondGroup personSecondGroup);
+    void Update(PersonSecondGroup personSecondGroup);
+}
