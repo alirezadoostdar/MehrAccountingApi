a9aefb2ceb6fa8c158346c35e1681cebc88ac9b6
alirezadoostdar <alireza.doostdar@gmail.com>
2026-03-30 21:37:35 +0200

    add peron dto

--- src/Core/Mehr.Application/Persons/Contracts/Dtos/AddPersonDto.cs
+++ src/Core/Mehr.Application/Persons/Contracts/Dtos/AddPersonDto.cs
@@ -1,5 +1,22 @@
-﻿namespace Mehr.Application.Persons.Contracts.Dtos;
+﻿using Mehr.Application.Contacts.Contracts.Dtos;
+using Mehr.Domain.Contacts;
+using Mehr.Domain.Entities.Costs;
+using Mehr.Domain.Entities.Persons;
+using Mehr.Domain.Persons;
+
+namespace Mehr.Application.Persons.Contracts.Dtos;
 
 public class AddPersonDto
 {
+    public string Title { get; set; }
+    public int? FirstGroupId { get; set; }
+    public int? SecondGroupId { get; set; }
+    public string Comment { get; set; }
+    public string Introducer { get; set; }
+    public long Code { get; set; }
+    public int SalePriceId { get; set; }
+    public AddContactDto? ContactInfo { get; set; }
+    public string ShopName { get; set; }
+    public PersonCustomerKind? PersonCustomerKind { get; set; }
+
 }
