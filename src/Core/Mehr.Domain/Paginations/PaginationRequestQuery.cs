e16d2301197844c1598a0e9883be2807396886bf
alirezadoostdar <alireza.doostdar@gmail.com>
2026-08-21 23:25:41 +0200

    refactor request query

--- src/Core/Mehr.Domain/Paginations/PaginationRequestQuery.cs
+++ src/Core/Mehr.Domain/Paginations/PaginationRequestQuery.cs
@@ -4,6 +4,6 @@ public class PaginationRequestQuery
 {
     public int Page { get; set; }
     public int PageSize { get; set; }
-    public string Search { get; set; }
+    public string? Search { get; set; }
 }
 
